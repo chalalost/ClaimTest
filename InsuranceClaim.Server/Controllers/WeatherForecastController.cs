5226bb84ca3a9728f50b5602832028e2469aed19
Tund <tutuan2682001@gmail.com>
2024-11-20 15:16:53 +0700

    commit

--- InsuranceClaim.Server/Controllers/WeatherForecastController.cs
+++ InsuranceClaim.Server/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace InsuranceClaim.Server.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
